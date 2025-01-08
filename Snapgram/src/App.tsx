081fe2cff7b3a16ed4c1bd84f204cf3cfc076562
Tega96 <otiteotega@gmail.com>
2025-01-08 04:46:17 +0100

    minor changes

--- Snapgram/src/App.tsx
+++ Snapgram/src/App.tsx
@@ -2,22 +2,24 @@ import { Routes, Route } from 'react-router-dom';
 import SignupForm from './_auth/form/SignupForm';
 import SigninForm from './_auth/form/SigninForm';
 import { Home } from './_root/pages';
-import './globals.css';
 import RootLayout from './_root/RootLayout';
+import AuthLayout from './_auth/AuthLayout';
+import './globals.css';
+
 
 
 const App = () => {
   return (
-    <main>
+    <main className="flex h-screen">
         <Routes>
             <Route element={<AuthLayout />}>
-                {/* private routes */}
+                {/* public routes */}
                 <Route path="sign-in" element={<SigninForm />} />
                 <Route path="sign-up" element={<SignupForm />} />
             </Route>
 
-            <Route element = {<RootLayout />}>
-                {/* public routes */}
+            <Route element={<RootLayout />}>
+                {/* private routes */}
                 <Route index element={<Home />} />
             </Route>
         </Routes>
