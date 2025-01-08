39b42c0e1edd804166a819c5814a3e0d20474fbc
Tega96 <otiteotega@gmail.com>
2025-01-08 03:32:49 +0100

    create forms and authLayout, create root page,

--- Snapgram/src/App.tsx
+++ Snapgram/src/App.tsx
@@ -1,10 +1,27 @@
-import { Router, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import SignupForm from './_auth/form/SignupForm';
+import SigninForm from './_auth/form/SigninForm';
+import { Home } from './_root/pages';
+import './globals.css';
+import RootLayout from './_root/RootLayout';
 
 
 const App = () => {
   return (
-    <Router>
-    </Router>
+    <main>
+        <Routes>
+            <Route element={<AuthLayout />}>
+                {/* private routes */}
+                <Route path="sign-in" element={<SigninForm />} />
+                <Route path="sign-up" element={<SignupForm />} />
+            </Route>
+
+            <Route element = {<RootLayout />}>
+                {/* public routes */}
+                <Route index element={<Home />} />
+            </Route>
+        </Routes>
+    </main>
   )
 }
 
