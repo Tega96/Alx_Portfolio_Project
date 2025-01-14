f779b61bd6edccf24014e8f36e47ee350942d1a3
Tega96 <otiteotega@gmail.com>
2025-01-14 19:18:06 +0100

    add more configuration to the AuthLayout

--- Snapgram/src/_auth/AuthLayout.tsx
+++ Snapgram/src/_auth/AuthLayout.tsx
@@ -1,19 +1,29 @@
 import { Outlet, Navigate } from 'react-router-dom';
 
+
+
 const AuthLayout = () => {
-    const isAuthenticated = false;
+    const isAuthenticated = true;
 
   return (
     <>
-        {isAuthenticated ? (
-            <Navigate to="/" />
-        ) : (
-            <>
-                <section>
-                    <Outlet />
-                </section>
-            </>
-        )}
+      {isAuthenticated ? (
+        <Navigate to="/" />
+    ): (
+        <>
+          <section className="flex flex-1 justify-center items-center py-10" >
+            <Outlet />
+
+            <img 
+            src="/asset/images/side-img.svg"
+            alt="logo"
+            className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat" 
+            />
+          </section>
+        </>
+
+
+      )}
     </>
   )
 }
