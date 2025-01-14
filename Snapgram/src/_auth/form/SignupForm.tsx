6193779134e790c885ca1b1ca3d1490b5bbd8bff
Tega96 <otiteotega@gmail.com>
2025-01-14 20:52:39 +0100

    add snapgram logo, style the form and add some more text

--- Snapgram/src/_auth/form/SignupForm.tsx
+++ Snapgram/src/_auth/form/SignupForm.tsx
@@ -37,6 +37,18 @@ const form = useForm<z.infer<typeof SignupValidation>>({
 
   return (
     <Form {...form}>
+      <div className="sm:w-420 flex-center flex-col">
+        <img src="/assets/images/logo.svg" alt="logo" />
+
+        <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">
+          Create a new account
+          <p className="text-light-3 small-medium md:base-regular mt-2"> To use Snapgram, 
+            enter your details
+          </p>
+        </h2>
+      
+
+
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
         <FormField
           control={form.control}
@@ -56,6 +68,7 @@ const form = useForm<z.infer<typeof SignupValidation>>({
         />
         <Button type="submit">Submit</Button>
       </form>
+      </div>
     </Form>
   )
 }
