1f738c80d88304383887a775be2863208b995d5e
Tega96 <otiteotega@gmail.com>
2025-01-15 04:01:15 +0100

    add the files for the homepage, define the various sections of the homepage

--- Snapgram/src/App.tsx
+++ Snapgram/src/App.tsx
@@ -4,6 +4,7 @@ import SignupForm from './_auth/form/SignupForm';
 import SigninForm from './_auth/form/SigninForm';
 import RootLayout from './_root/RootLayout';
 import { Home } from './_root/pages';
+// import { Toaster } from "@/components/ui/toaster";
 
 import './globals.css';
 
@@ -24,6 +25,8 @@ const App = () => {
                 <Route index element={<Home />} />
             </Route>
         </Routes>
+
+        {/* <Toaster /> */}
     </main>
   )
 }
--- Snapgram/src/_root/RootLayout.tsx
+++ Snapgram/src/_root/RootLayout.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import Topbar from '@/components/ui/shared/Topbar';
+import LeftSidebar from '@/components/ui/shared/LeftSidebar';
+import Bottombar from '@/components/ui/shared/Bottombar';
+import { Outlet } from 'react-router-dom';
 
 const RootLayout = () => {
   return (
-    <div>RootLayout</div>
+    <div className="w-full md:flex">
+      <Topbar />
+      <LeftSidebar />
+
+      <section className="flex flex-1 h-full">
+        <Outlet />
+      </section>
+
+      
+      <Bottombar />
+
+
+    </div>
   )
 }
 
